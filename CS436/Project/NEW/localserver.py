e30ebfcd84450d6fb1a929e3b951f2a841f6ab43
altoveros <62730844+altoveros@users.noreply.github.com>
2020-03-26 21:30:39 -0700

    Add files via upload

---
+++ CS436/Project/NEW/localserver.py
@@ -0,0 +1,78 @@
+from socket import *
+
+RRTable = {
+    "number1":
+    {
+        "TransactionID": "1",
+        "Name": "www.csusm.edu",
+        "Type": "A",
+        "Value": "144.37.5.45",
+        "TTL": "",
+        "Static" : "1"
+        }
+    "number2":
+    {
+        "TransactionID": "2",
+        "Name": "cc.csusm.edu",
+        "Type": "A",
+        "Value": "144.37.5.117",
+        "TTL": "",
+        "Static" : "1"
+        }
+    "number3":
+    {
+        "TransactionID": "3",
+        "Name": "cc1.csusm.edu",
+        "Type": "CNAME",
+        "Value": "cc.csusm.edu",
+        "TTL": "",
+        "Static" : "1"
+        }
+    "number4":
+    {
+        "TransactionID": "4",
+        "Name": "cc1.csusm.edu",
+        "Type": "A",
+        "Value": "144.37.5.118",
+        "TTL": "",
+        "Static" : "1"
+        }
+    "number5":
+    {
+        "TransactionID": "5",
+        "Name": "my.csusm.edu",
+        "Type": "A",
+        "Value": "144.37.5.150",
+        "TTL": "",
+        "Static" : "1"
+        }
+    "number6":
+    {
+        "TransactionID": "6",
+        "Name": "www.qualcomm.com",
+        "Type": "NS",
+        "Value": "dns.qualcomm.com",
+        "TTL": "",
+        "Static" : "1"
+        }
+    "number7":
+    {
+        "TransactionID": "7",
+        "Name": "viasat.com",
+        "Type": "NS",
+        "Value": "dns.viasat.com",
+        "TTL": "",
+        "Static" : "1"
+        }
+    }
+
+
+
+serverPort = 15000
+serverSocket = socket(AF_INET, SOCK_DGRAM)
+serverSocket.bind(('', serverPort))
+print ('The server is ready to receive')
+while 1:
+    message, clientAddress = serverSocket.recvfrom(2048)
+    modifiedMessage = message.decode().upper()
+    serverSocket.sendto(modifiedMessage.encode(), clientAddress)
---
+++ CS436/Project/NEW/test.py
@@ -0,0 +1,45 @@
+from socket import *
+
+
+serverName = 'localserver'
+serverPort = 15000
+clientSocket = socket(AF_INET, SOCK_DGRAM)
+
+RRTableName = []
+RRTableType = []
+RRTableValue = []
+RRTTLValue = []
+RRStaticValue = []
+
+
+count = 1
+
+Flag = True
+
+
+while Flag:
+    
+    name = input('Enter the host or domain name (Exit to quit program): ' )
+
+    if name == "exit":
+        Flag = False
+    else:
+        DNSQuery = input('Enter the type of DNS query (0. A, 1. AAAA, 2. CNAME, 3. NS: ')
+
+        # Check to see if the name is already in the dictionary.
+        if name in RRTableName:
+            print("\n")
+            print(name + " already exists, here is the current table")
+            print("\n")
+            print("Name\t\tType\t\tValue\t\tTTL\t\tStatic")
+            for x in range(len(RRTableName)):
+                print(RRTableName[x] +  "\t\t" + RRTableType[x])
+               
+            
+            
+        else:
+            clientSocket.sendto(name.encode(), (serverName, serverPort))
+            DNSResponse, serverAddress = clientSocket.recvfrom(2048)
+            RRTableName.append(DNSResponse.decode())
+            print('{the_name} has been added.'.format(the_name = name))
+
