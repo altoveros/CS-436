d68c35ba65e55c10d4dcb2a960d16850ba4b9e83
AndrewWMarston <60202362+AndrewWMarston@users.noreply.github.com>
2020-03-26 19:34:52 -0700

    Update test.py

--- CS436/Project/test.py
+++ CS436/Project/test.py
@@ -1,7 +1,10 @@
 from socket import *
 import numpy as np
 
-clientSocket = socket(AF_INET, SOCK_DGRAM)
+  clientSocket = socket(AF_INET, SOCK_DGRAM)
+
+RRTable{}
+count = 0
 
 message = input('Enter the host or domain name: ')
 
@@ -11,8 +14,15 @@ message2 = input('Enter the type of DNS query (0. A, 1. AAAA, 2. CNAME, 3. NS: '
 
 clientSocket.sendto(message.encode(),(serverName, serverPort))
 modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
-print (modifiedMessage.decode())
+print(modifiedMessage.decode())
 
+if not message in RRTable.values():
+  RRTable[key].append('number ', count)
+  RRTable[key].append(message)
+  RRTable[key].append(message2)
+  RRTable[key].append(serverAddress)
+  RRTable[key].append(60)
+  RRTable[key].append()
 
 clientSocket.close()
 
